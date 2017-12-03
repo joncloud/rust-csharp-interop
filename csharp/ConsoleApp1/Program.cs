7c0dcd0cd47b4399df9e95883fcef0bbd8bad55c
joncloud <jdberube@gmail.com>
2017-12-03 15:01:14 -0800

    Exports impl functions

--- csharp/ConsoleApp1/Program.cs
+++ csharp/ConsoleApp1/Program.cs
@@ -12,22 +12,68 @@ namespace ConsoleApp1
             var number = RustLib.MaybeParse("123").ToOption().UnwrapOr(-1);
             Console.WriteLine(number);
             number = RustLib.MaybeParse("abc").ToOption().UnwrapOr(-1);
+
             Console.WriteLine(number);
+
+            var vec = RustLib.MaybePos().ToOption().Unwrap();
+            Console.WriteLine(vec);
+            Console.WriteLine(vec.Magnitude());
         }
     }
 
-    struct OptionInt
+    struct Vector3
     {
-        public byte IsSome { get; private set; }
-        public int Value { get; private set; }
+        public readonly float X;
+        public readonly float Y;
+        public readonly float Z;
+
+        public Vector3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public float Magnitude() =>
+            RustLib.Magnitude(this);
+
+        public override string ToString() =>
+            $"({X}, {Y}, {Z})";
+    }
+
+    static class RustOption
+    {
+        public static Option<T> ToOption<T>(byte isSome, T value) =>
+            isSome == 1 ? Some(value) : None<T>();
+    }
+
+    struct RustOptionInt32
+    {
+        public readonly byte IsSome;
+        internal readonly int Value;
 
         public Option<int> ToOption() =>
-            IsSome == 1 ? Some(Value) : None<int>();
+            RustOption.ToOption(IsSome, Value);
+    }
+
+    struct RustOptionVector3
+    {
+        public readonly byte IsSome;
+        internal readonly Vector3 Value;
+
+        public Option<Vector3> ToOption() =>
+            RustOption.ToOption(IsSome, Value);
     }
 
     static class RustLib
     {
         [DllImport("lib", EntryPoint = "maybe_parse")]
-        public extern static OptionInt MaybeParse(string text);
+        public extern static RustOptionInt32 MaybeParse(string text);
+
+        [DllImport("lib", EntryPoint = "maybe_pos")]
+        public extern static RustOptionVector3 MaybePos();
+
+        [DllImport("lib", EntryPoint = "magnitude")]
+        public extern static float Magnitude(Vector3 vector);
     }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -14,3 +14,26 @@ pub extern fn maybe_parse(s: *const c_char) -> Option<i32> {
     let text = c_str.to_str().unwrap();
     text.parse().ok()
 }
+
+pub struct Vector3 {
+    x: f32,
+    y: f32,
+    z: f32
+}
+
+impl Vector3 {
+    #[no_mangle]
+    pub extern fn magnitude(&self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+}
+
+#[no_mangle]
+pub extern fn maybe_pos() -> Option<Vector3> {
+    let vector = Vector3{
+        x: 1.0,
+        y: 2.0,
+        z: 3.0
+    };
+    Some(vector)
+}
